ca688048ec9cea14179d8fb7d47e0ec323f43263
contactharrisc2 <contactharrisc2@gmail.com>
2022-04-13 03:14:13 -0500

    capitalized first letter for all drinks

--- server.js
+++ server.js
@@ -5,24 +5,52 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
+
 //////////////////////
 // Database
 //////////////////////
-
-//////////////////////
+const drinks = require("./models/drinks");
+capitalize(drinks);
+/////////////////////
 // Declare Middleware
 //////////////////////
-
+app.use("/static",express.static("public"));
 
 ///////////////////////
 // Declare Routes and Routers 
 ///////////////////////
+
+//root
 app.get("/", (req, res) =>{
     res.send("Welcome to the Gitpub App!")
 });
+
+
+//index
+app.get("/drinks", (req, res) =>{
+    res.render("drinks_index.ejs",{drinks: drinks});
+});
+
+
 ///////////////////////////
 // Server Listener
 ///////////////////////////
 app.listen(PORT, ()=>{
     console.log("drinking at port", PORT);
 });
+
+///////////////////////////
+// Functions
+///////////////////////////
+function capitalize(words){
+    let letters;
+    let capital;
+    for(let i = 0; i < words.length; i++){
+        capital = words[i].name[0].toUpperCase()
+        letters = words[i].name.split("")
+        letters.splice(0,1,capital)
+        words[i].name = letters.join("")
+        letters = null
+        // console.log(words[i].name)
+    }
+}
